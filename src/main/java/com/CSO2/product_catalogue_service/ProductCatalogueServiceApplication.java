2c4e7ec82d3be2d356412b212e0d752cc3ebc763
moonlander101 <himathsamarakoon@gmail.com>
2025-12-05 01:39:26 +0530

    fix: Testing out a data seeding solution

--- src/main/java/com/CSO2/product_catalogue_service/ProductCatalogueServiceApplication.java
+++ src/main/java/com/CSO2/product_catalogue_service/ProductCatalogueServiceApplication.java
@@ -2,8 +2,10 @@ package com.CSO2.product_catalogue_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class ProductCatalogueServiceApplication {
 
 	public static void main(String[] args) {
